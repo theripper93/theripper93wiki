8a3034014fec9b038e5aa1025c634b64f9a81a3e
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-12-07 23:28:59 +0100

    Organizing

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/theripper93',
   },
   chat: {
     link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
   },
 }
 
