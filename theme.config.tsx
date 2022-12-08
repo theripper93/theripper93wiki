c6fe04ff1214347fe9ea8eb94f1ee2ef6a55ca1b
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-12-08 13:58:36 +0100

    Folder fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>theripper93</span>,
   project: {
     link: 'https://github.com/theripper93',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/F53gBjR97G',
   },
   footer: {
-    text: "Created by theripper93 and apriltaoyvr with nextra"
+    text: 'Created by theripper93 and apriltaoyvr with nextra',
   },
-  primaryHue: 47,
-  head: <script src="https://kit.fontawesome.com/fc91175617.js" crossOrigin="anonymous"></script>
+  primaryHue: 45,
+  head: (
+    <>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta property='og:title' content='theripper93 wiki' />
+      <meta property='og:description' content='The wiki for theripper93' />
+      <script
+        src='https://kit.fontawesome.com/fc91175617.js'
+        crossOrigin='anonymous'
+      ></script>
+    </>
+  ),
 };
 
-export default config
+export default config;
