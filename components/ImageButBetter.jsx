b41fbb46ae678de5c0bcb3034723a4d96e7635f9
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-12-09 18:29:40 +0100

    Lint

--- components/ImageButBetter.jsx
+++ components/ImageButBetter.jsx
@@ -1,16 +1,11 @@
-import Image from 'next/image'
-import styles from '../pages/globals.module.css'
+import Image from 'next/image';
+import styles from '../pages/globals.module.css';
 
 export default function ImageButBetter({ src, alt, ...props }) {
-    return (
-        <div className={props.className}>
-            <span className={styles.wikidesc}>{alt}</span>
-            <Image
-            src={src}
-            alt={alt}
-            width={2000}
-            height={2000}
-            />
-        </div>
-    )
+  return (
+    <div className={props.className}>
+      <span className={styles.wikidesc}>{alt}</span>
+      <Image src={src} alt={alt} width={2000} height={2000} />
+    </div>
+  );
 }
