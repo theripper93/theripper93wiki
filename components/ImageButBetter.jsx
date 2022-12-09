1236852c18ce34d1602bb63c22671372685b11d4
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-12-09 20:22:04 +0100

    Remove eslint

--- components/ImageButBetter.jsx
+++ components/ImageButBetter.jsx
@@ -2,16 +2,18 @@ import Image from 'next/image';
 import styles from '../pages/globals.module.css';
 
 export default function ImageButBetter({ src, alt, ...props }) {
-    return (
-        <div className={props.className}>
-            <span className={styles.wikidesc}>{alt}</span>
-            <Image
-            src={src}
-            alt={alt}
-            width={2000}
-            height={2000}
-            className={props.className == styles.wikiimage ? styles.wikiimg : styles.wikiimg}
-            />
-        </div>
-    )
+  return (
+    <div className={props.className}>
+      <span className={styles.wikidesc}>{alt}</span>
+      <Image
+        src={src}
+        alt={alt}
+        width={2000}
+        height={2000}
+        className={
+          props.className == styles.wikiimage ? styles.wikiimg : styles.wikiimg
+        }
+      />
+    </div>
+  );
 }
