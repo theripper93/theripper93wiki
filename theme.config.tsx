661fe130ae238f0bd7d1aa0b9424224abc260ae1
theripper93 <tsplab@gmail.com>
2022-12-10 16:21:29 +0100

    a

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,7 +5,7 @@ import { config as faConfig, dom } from '@fortawesome/fontawesome-svg-core';
 faConfig.autoAddCss = false;
 
 const config: DocsThemeConfig = {
-  logo: <span>✨</span>,
+  logo: <span>TheRipper93's WIKI</span>,
   project: {
     link: 'https://github.com/theripper93',
   },
@@ -15,6 +15,7 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Created by theripper93 and apriltaoyvr with Nextra',
   },
+  titleSuffix: ' - TheRipper93 WIKI',
   primaryHue: 45,
   head: () => {
     const { frontMatter } = useConfig();
@@ -23,18 +24,18 @@ const config: DocsThemeConfig = {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta
           property='og:title'
-          content={frontMatter.title || '✨ TheRipper93'}
+          content={'✨ TheRipper93'}
         />
         <meta
           property='og:description'
-          content={frontMatter.description || 'The wiki for TheRipper93'}
+          content={'The wiki for TheRipper93'}
         />
         <link rel='icon' type='image/png' href='/static/images/favicon.png' />
         <style>{dom.css()}</style>
       </>
     );
   },
-  faviconGlyph: '✨',
+  faviconGlyph: '✨',//https://theripper93.com/assets/favicon.ico
 };
 
 export default config;
