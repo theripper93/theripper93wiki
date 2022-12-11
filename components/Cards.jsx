cfd60733f4008d7d9959597e00fddee07978da73
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-12-12 00:39:58 +0100

    Restore

---
+++ components/Cards.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import styles from './cards.module.css';
+
+export default function Cards({ cards }) {
+    return (
+        <div className={styles.cardcontainer}>
+        {cards.map((card) => (
+            <Card key={card.title} {...card} />
+        ))}
+        </div>
+    );
+}
+
+function Card({ icon, title, href }) {
+    return (
+        <Link href={href ?? './'} className={styles.card}>{title}</Link>
+    );
+}
