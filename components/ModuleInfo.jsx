ee086375fbbae636e341f59d1e90ef8dfd1b073d
April <95392008+apriltaoyvr@users.noreply.github.com>
2022-12-12 01:36:37 +0100

    Buttons

--- components/ModuleInfo.jsx
+++ components/ModuleInfo.jsx
@@ -1,38 +1,44 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import styles from './moduleinfo.module.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import styles from './moduleinfo.module.css';
 
 export default function ModuleInfo({ moduleId }) {
+  async function fetchData() {
+    return await fetch(`https://forge-vtt.com/api/bazaar/package/${moduleId}`)
+      .then((response) => response.json())
+      .then((data) => data);
+  }
 
+  const [module, setModule] = useState({});
 
-    async function fetchData(){
-        return await fetch(
-            `https://forge-vtt.com/api/bazaar/package/${moduleId}`
-          )
-            .then((response) => response.json())
-            .then((data) => data);
-    }
+  useEffect(() => {
+    fetchData().then((data) => {
+      if (data.success) setModule(data.package);
+    });
+  }, []);
 
-    const [module, setModule] = useState({});
-
-    useEffect(() => {
-        fetchData().then(data => {
-            if(data.success) setModule(data.package)});
-    }, []);
-
-    return (
-        <div className={styles.infowrapper} style={module.installs ? {} : {display: "none"}}>
-            <ModuleInfoButton name={'Version: ' + module.latest} color={'red'} />
-            <ModuleInfoButton name={'Installs: ' + module.installs + '%'} color={'green'} />
-            <ModuleInfoButton name={'FVTT: V10'} color={'orange'}/>
-        </div>
-    )
+  return (
+    <div
+      className={styles.infowrapper}
+      style={module.installs ? {} : { display: 'none' }}
+    >
+      <ModuleInfoButton name={'Version: ' + module.latest} color={'#ef4444'} />
+      <ModuleInfoButton
+        name={'Installs: ' + module.installs + '%'}
+        color={'#22c55e'}
+      />
+      <ModuleInfoButton name={'FVTT: V10'} color={'#f97316'} />
+    </div>
+  );
 }
 
-function ModuleInfoButton({ name, color}){
-    return (
-        <div className={styles.modulebutton} style={{backgroundColor: color || 'var(--nextra-primary-hue)' }}>
-            {name}
-        </div>
-    )
+function ModuleInfoButton({ name, color = 'var(--nextra-primary-hue)' }) {
+  return (
+    <div
+      style={{ color: color}}
+      className={styles.modulebutton}
+    >
+      {name}
+    </div>
+  );
 }
