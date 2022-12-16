20b27ae8e3e723327ca165905c3e402a2828676f
theripper93 <tsplab@gmail.com>
2022-12-16 16:24:25 +0100

    a

--- pages/404.js
+++ pages/404.js
@@ -6,12 +6,6 @@ import styles from './404.module.css'
 const fontfamily = 'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Noto Sans,Ubuntu,Cantarell,Helvetica Neue,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji'
  
 export default function Custom404() {
-    useEffect(() => {
-        setTimeout(() => {
-            window.location.href = '/'
-        }, 5000)
-    }, [])
-
     return <div className={styles.rainbow} style={{fontFamily: fontfamily, color: 'white', position: 'absolute', left: 0, top: 0, width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
         <div>
         <h1 class="next-error-h1" style={{ textShadow: '0 0 2px black',mixBlendMode: 'overlay', borderRight: '1px solid white', display:'inline-block', margin:'0', marginRight:'20px', padding:'0 23px 0 0', fontSize:'24px', fontWeight:'500', verticalAlign:'top', lineHeight:'49px'}}>404 🙃</h1>
