0775fc8dafc706e9d821feabf02dd04e51d12feb
theripper93 <tsplab@gmail.com>
2022-12-18 14:48:26 +0100

    a

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,7 +5,7 @@ import { config as faConfig, dom } from '@fortawesome/fontawesome-svg-core';
 faConfig.autoAddCss = false;
 
 export default {
-  logo: <div style={{display: 'flex',alignItems: 'center',gap: '0.5rem'}}><img src="https://theripper93.com/assets/favicon.ico" style={{width: '2rem', height: '2rem'}} alt="" /> <span> TheRipper93's Wiki</span></div>,
+  logo: <div style={{display: 'flex',alignItems: 'center',gap: '0.5rem'}}><img src="/images/static/favicon.ico" style={{width: '2rem', height: '2rem'}} alt="" /> <span> TheRipper93's Wiki</span></div>,
   project: {
     link: 'https://github.com/theripper93',
   },
