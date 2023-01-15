1fac24794069edefd165dc1ed7c9832bec59ab9d
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-15 17:56:37 +0100

    Rename ImageButBetter to Image; clean up

---
+++ components/Image.tsx
@@ -0,0 +1,35 @@
+import NextImage from 'next/image';
+import styles from './image.module.css';
+import { useState } from 'react';
+
+export default function Image({ src, alt, className, width = 2000, height = 2000 }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div
+      className={className}
+      style={
+        expanded
+          ? { width: '100%', transform: 'scale(1)', transitionDuration: '0ms' }
+          : {}
+      }
+    >
+      <span className={styles.wikidesc}>{alt}</span>
+      <NextImage
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        onClick={() => setExpanded(!expanded)}
+        style={
+          expanded
+            ? { width: '100%', maxHeight: 'unset', maxWidth: 'unset' }
+            : {}
+        }
+        className={
+          className == styles.wikiimage ? styles.wikiimg : styles.wikiimgsmall
+        }
+      />
+    </div>
+  );
+}
--- components/ModuleInfo.jsx
+++ components/ModuleInfo.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { useRouter } from "next/router";
 import styles from './moduleinfo.module.css'
 
