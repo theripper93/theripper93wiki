99b7ec48bbb5e22635953c2f21b5f4750ca8b24f
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-15 19:31:14 +0100

    No more inline styles

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,54 +1,19 @@
-// Inside the "pages/404.js" file add the below code
-import styles from './404.module.css'
+import styles from './404.module.css';
+import Router, { useRouter } from 'next/router';
 
 export default function Custom404() {
-    return (
-      <div className={styles.rainbow}>
-        <div>
-          <h1 className='next-error-h1 heading'>404 🙃</h1>
-          <div
-            className={styles.subheadingContainer}
-          >
-            <h2
-              style={{
-                fontSize: '14px',
-                fontWeight: 'normal',
-                lineHeight: '49px',
-                margin: 0,
-                padding: 0,
-              }}
-            >
-              You f*cked up lmao.
-            </h2>
-          </div>
+  const router = useRouter()
+  return (
+    <div className={styles.rainbow}>
+      <div>
+        <h1 className={styles.heading}>404 🙃</h1>
+        <div className={styles.subheadingContainer}>
+          <h2 className={styles.subheading}>You f*cked up lmao.</h2>
         </div>
-        <a
-          href='/'
-          className={styles.text}
-          style={{
-            textShadow: '0 0 2px black',
-            mixBlendMode: 'overlay',
-            textDecoration: 'none',
-            marginTop: '1rem',
-            display: 'inline-block',
-            textAlign: 'left',
-            lineHeight: '49px',
-            height: '49px',
-            verticalAlign: 'middle',
-          }}
-        >
-          <h3
-            style={{
-              color: 'white !important',
-              fontWeight: 'normal',
-              lineHeight: '49px',
-              margin: 0,
-              padding: 0,
-            }}
-          >
-            Go back home →
-          </h3>
-        </a>
       </div>
-    );
+      <div onClick={() => router.back()} className={styles.returnContainer}>
+        <h3 className={styles.return}>Go back home →</h3>
+      </div>
+    </div>
+  );
 }
